a218a67f25167f45439d7e361b5eebc6eca638c1
firelizardback <firelizardback@gmail.com>
2021-11-13 22:37:15 -0600

    FPGA moved to a new repository

--- FPGA/Partial_reconfiguration/Two_modules_hardware_test/multi32_float.ip_user_files/sim_scripts/vio_0/xcelium/run.f
+++
@@ -1,4 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../multi32_float.srcs/sources_1/ip/vio_0/sim/vio_0.vhd" \
--endlib
-
--- FPGA/Partial_reconfiguration/top/top.ip_user_files/sim_scripts/vio_test/ies/run.f
+++
@@ -1,4 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../top.srcs/sources_1/ip/vio_test/sim/vio_test.vhd" \
--endlib
-
